df1942a70a3d007883c374b712a9dac197717528
Matiusha Oleksandr <oleksandr@gmail.com>
2020-01-23 23:57:01 +0200

    4

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello ");
         System.out.println("Hello 2");
+        System.out.println("Hello 3");
     }
 
 }
